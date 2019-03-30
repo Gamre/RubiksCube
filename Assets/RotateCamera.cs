77f07fd5c7ad6652d9e1d46d5d39bdf67f7aaf33
Gamre <jorgengamre@gmail.com>
2019-03-30 12:14:36 +0100

    Initial commit

---
+++ Assets/RotateCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+public KeyCode pressLefttest;
+public KeyCode pressRight;
+public KeyCode pressUp;
+public KeyCode pressDown;
+
+public float speedH = 1.0f;
+public float speedV = 2.0f;
+
+private float yaw = 0.0f;
+private float tilt = 0.0f;
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButton("pressLeft")) {
+			yaw += speedH * 2;			
+			transform.eulerAngles = new Vector3(0.0f, yaw, tilt);
+		}
+		if(Input.GetButton("pressRight")) {
+			yaw -= speedH * 2;			
+			transform.eulerAngles = new Vector3(0.0f, yaw, tilt);
+		}
+		if(Input.GetButton("pressDown")) {
+			tilt += speedV * 2;	
+			if (tilt > 70) {
+				tilt = 70;
+			}
+			transform.eulerAngles = new Vector3(0.0f, yaw, tilt);
+		}
+		if(Input.GetButton("pressUp")) {
+			tilt -= speedV * 2;	
+			if (tilt < -30.0f) {
+				tilt = -30.0f;
+			}		
+			transform.eulerAngles = new Vector3(0.0f, yaw, tilt);
+		}
+			
+
+	}
+}
---
+++ Assets/Selected.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selected : MonoBehaviour {
+	[SerializeField]
+	public Material[] materials;
+	public Renderer rend;
+
+	private int index = 1;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown() {
+		if(Input.GetMouseButtonDown (0)) {
+			index += 1;
+			if(index == materials.Length+1) {
+				index = 1; 
+			}
+			rend.sharedMaterial = materials[index-1];
+		}
+		
+	}
+}
